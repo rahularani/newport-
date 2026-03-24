2797518f236970eb7dd05fad62466181a10ba7bb
Rahul R <rahulramesharani@gmail.com>
2026-03-24 17:57:49 +0530

    Configure Express to serve React build for single platform deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +11,14 @@ app.use(express.json());
 
 // Routes
 app.use("/api/contact", require("./routes/contact"));
+
+// Serve React build in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  });
+}
 
 // Connect to MongoDB and start server
 mongoose
